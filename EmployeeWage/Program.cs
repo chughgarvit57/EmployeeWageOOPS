89d7057a1ece24efe5fc3da636a69c366b50e58d
chughgarvit57 <gavi.chugh@gmail.com>
2025-02-21 19:38:39 +0530

    usecase-1 code pushed

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, This is the dev branch of my project. Please check different branches for different usecases.");
+            // Use case 1 - Check whether employee is present or not
+            Console.Write("Enter no. of hours you worked today (max-8) : ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            if(hours > 8)
+            {
+                Console.WriteLine("Invalid Input.");
+            } else if(hours >= 6 && hours <= 8)
+            {
+                Console.WriteLine("Employee is present.");
+            } else
+            {
+                Console.WriteLine("Employee is absent.");
+            }
         }
     }
 }
