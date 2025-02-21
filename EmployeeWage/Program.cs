6e7d0aabcab8806ebb5928ac7105c2c5ca478f6e
chughgarvit57 <gavi.chugh@gmail.com>
2025-02-21 23:23:02 +0530

    usecases-4 codes pushed

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            
+            // Usecase-4 calculating employee wage using switch-case statements
+            Console.Write("Enter no. of hours(4 or 8 only): ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            int empWage = 0;
+            int emp_rate_per_hour = 20;
+            switch (hours)
+            {
+                case 4:
+                    Console.WriteLine("You did part-time work.");
+                    empWage = hours * emp_rate_per_hour;
+                    break;
+                case 8:
+                    Console.WriteLine("Congratulations! You completed your full day's work.");
+                    empWage = hours * emp_rate_per_hour;
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input.");
+                    break;
+            }
+            Console.WriteLine($"Your total wage is Rs {empWage}/-");
         }
     }
 }
