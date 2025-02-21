e165457f3d3106c7cdc1bf53ff175a759020977f
chughgarvit57 <gavi.chugh@gmail.com>
2025-02-21 20:10:18 +0530

    usecase-2 code pushed

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -5,6 +5,25 @@
         static void Main(string[] args)
         {
             Console.WriteLine("This is the main/dev branch of my project. CHeck different branches for different use-cases.");
+            // usecase-2 calculating employee wage based on their presence
+            Console.Write("Enter no. of hours you worked (max-8): ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+            int empWage;
+            int emp_rate_per_hour = 30;
+            if(hours > 8)
+            {
+                Console.WriteLine("Invalid Input.");
+                empWage = 0;
+            } else if(hours >= 5 && hours <= 8)
+            {
+                Console.WriteLine("Congratulations! Your work was appreciated.");
+                empWage = hours * emp_rate_per_hour;
+            } else
+            {
+                Console.WriteLine("Sorry! You have not worked to that extent as expected.");
+                empWage = 0;
+            }
+            Console.WriteLine($"You total wage is: Rs {empWage}/-");
         }
     }
 }
